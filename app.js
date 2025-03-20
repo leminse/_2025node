cd0b6e6a8c986508fc60c0cc7dcd93fa8e975917
Another Name <s2330@e-mirim.hs.kr>
2025-03-21 08:58:37 +0900

    post 요청

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const express = require('express');
 const app = express();
 
 // 루트 경로('/')에 대한 GET 요청을 처리합니다.
-app.get('/', (req, res) => {
+app.get('/swag', (req, res) => {
   // 응답 본문에 'Hello, World!'를 보냅니다.
-  res.send('Hello, World!');
+  res.send('get swag');
+});
+
+app.post('/swag', (req, res) => {
+  // 응답 본문에 'Hello, World!'를 보냅니다.
+  res.send('post swag');
 });
 
 // 서버가 포트 3000에서 요청을 대기합니다.
