fba419bbe20229acd9b9f95e2d0a3c99dd31b34c
Another Name <s2330@e-mirim.hs.kr>
2025-03-21 10:01:28 +0900

    Router로 파일을 별도 관리

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-// express 모듈을 가져옵니다.
 const express = require('express');
+const swagRouter = require('./routes/swag');
 
-// express 애플리케이션을 생성합니다.
 const app = express();
 
 app.use(express.json());
+app.use('/swag', swagRouter);
 
-// 루트 경로('/')에 대한 GET 요청을 처리합니다.
 app.get('/swag', (req, res) => {
-  // 응답 본문에 'Hello, World!'를 보냅니다.
   res.send('get swag');
 });
 
-app.post('/swag', (req, res) => {
-  // 응답 본문에 'Hello, World!'를 보냅니다.
-  res.send(req.body);
-  // 의심 되는 코드에 F9해서 돌리기
+app.get('/swag/:person', (req, res) => {
+  res.send(req.params.person);
 });
 
-// 서버가 포트 3000에서 요청을 대기합니다.
+app.post('/swag', (req, res) => {
+  res.send(req.body);
+});
+
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000에서 실행 중입니다.');
 });
