7dcc0f4994549c59c55566a68f5e01ffce0f4243
Another Name <s2330@e-mirim.hs.kr>
2025-03-31 10:36:48 +0900

    Read(Get)완성

--- app1.js
+++ app1.js
@@ -1,23 +1,14 @@
+// express 모듈을 가져옵니다.
 const express = require('express');
 const swagRouter = require('./routes/swag');
 
+// express 애플리케이션을 생성합니다.
 const app = express();
 
 app.use(express.json());
 app.use('/swag', swagRouter);
 
-app.get('/swag', (req, res) => {
-  res.send('get swag');
-});
-
-app.get('/swag/:person', (req, res) => {
-  res.send(req.params.person);
-});
-
-app.post('/swag', (req, res) => {
-  res.send(req.body);
-});
-
+// 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000에서 실행 중입니다.');
 });
--- app2.js
+++ app2.js
@@ -5,27 +5,28 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-0
+
+//.env로 민감한 데이터 이동
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
 });
 
-travelList = ['뉴욕', '파리', '서울', '도쿄'];
+travelList = ['뉴욕', '파리', '서울', '도쿄']
 
-app.set('view engine','ejs');
-//__dirname : 현재 파일이 속한 절대경로
-//path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다.
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// _difname : 현재 파일이 속한 절대 경로
+// path.join을 사용하면 운영체제에 맞추어 경로 구붅자(/,\)를 알아서 정해준다.
+console.log('views', path.join(__dirname, 'views'));
 
-db.connect(err=> {
-  if(err){
-    console.err('MySQL 연결 실패', err); 
+db.connect(err => {
+  if(err) {
+    console.log('MySQL 연결 실패 : ', err);
     return;
   }
-  console.log('MySQL에 연결되었습니다');
+  console.log('MySQL에 연결되었습니다.');
 });
 
 app.get('/', (req, res) => {
@@ -33,10 +34,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/travel', (req, res) => {
-    res.render('travel', {travelList})
-});
-
-//서버가 포트 3000에서 요청을 대기합니다,
-app.listen(3001, () => {
-    console.log('서버가 http://localhost:3001에서 실행 중입니다.');
+  const query = 'SELECT id, name FROM travellist';
+  db.query(query, (err, results) => {
+    if(err) {
+      console.error('데이터베이스 쿼리 실패');
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const travelList = results;
+    res.render('travel', {travelList});
   });
+});
+app.get('/travel/:id', (req, res) => {
+  const travelID = req.params.id;
+  const query = 'SELECT * FROM travellist WHERE id =?';
+  db.query(query, [travelID], (err, results) => {
+    if(err) {
+      console.error('DB쿼리 실패', err);
+      res.status(500).send('내부 서버 에러');
+      return;
+    }
+    if(results.length === 0) {
+      res.status(404).send('여행지를 찾을 수 없습니다.');
+      return;
+    }
+    const travel = results[0];
+    res.render('travelDetail', {travel});
+  }) 
+})
+
+// 서버가 포트 3000에서 요청을 대기합니다.
+app.listen(3000, () => {
+  console.log('서버가 http://localhost:3000에서 실행 중입니다.');
+});
