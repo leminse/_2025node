c723dd3e71cabb52ad28ccfe7686a4f0bd9dc2e9
Another Name <s2330@e-mirim.hs.kr>
2025-03-31 09:58:34 +0900

    Read(Get)

--- app2.js
+++ app2.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-
+0
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -18,11 +18,11 @@ travelList = ['뉴욕', '파리', '서울', '도쿄'];
 app.set('view engine','ejs');
 //__dirname : 현재 파일이 속한 절대경로
 //path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다.
-app.set('views', path.join(__dirname), 'views');
+app.set('views', path.join(__dirname, 'views'));
 
 db.connect(err=> {
   if(err){
-    console.err('MySQL 연결 실패', err);
+    console.err('MySQL 연결 실패', err); 
     return;
   }
   console.log('MySQL에 연결되었습니다');
